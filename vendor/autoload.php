065feb3574239cf65d8733ec509e1cd130114159
JoshyPHP <s9e.dev@gmail.com>
2015-11-19 15:20:03 +0100

    Updated vendor

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitfd25eded335afc3c52d088dcb4a6cde5::getLoader();
+return ComposerAutoloaderInitfa88b22c32b34757967df47c66af38f8::getLoader();
